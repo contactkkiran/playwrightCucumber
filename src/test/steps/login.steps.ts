50e7470e3e1d42c159e0b9339ac1011765e5bdc2
Kiran <contactkkiran@gmail.com>
2024-06-14 12:06:09 +0530

    First commit

---
+++ src/test/steps/login.steps.ts
@@ -0,0 +1,6 @@
+import { Given } from "@cucumber/cucumber";
+
+Given('User navigates to the application', function () {
+           // Write code here that turns the phrase above into concrete actions
+          console.log("Hi Iam implemented")
+         });
