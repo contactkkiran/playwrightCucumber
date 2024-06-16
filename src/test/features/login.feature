667b1dc94bd665deb225f1c82f36091a15b55166
Kiran <contactkkiran@gmail.com>
2024-06-17 04:17:16 +0530

    add  BDD Steps

--- src/test/features/login.feature
+++ src/test/features/login.feature
@@ -5,13 +5,13 @@ Feature: User Authentication tests
     And User click on the login link
 
   Scenario: Login should be success
-    And User enter the username as "ortoni11"
-    And User enter the password as "Pass1234"
+    And User enter the username as "contactkkiran@gmail.com"
+    And User enter the password as "Varma@143"
     When User click on the login button
     Then Login should be success
 
   Scenario: Login should not be success
-    Given User enter the username as "koushik"
-    Given User enter the password as "Passkoushik"
+    Given User enter the username as "Dummy@gmail.com"
+    Given User enter the password as "Dummy"
     When User click on the login button
-    But Login should fail
+    But Login should fail and should show error message
--- src/test/steps/login.steps.ts
+++ src/test/steps/login.steps.ts
@@ -1,6 +1,95 @@
-import { Given } from "@cucumber/cucumber";
+import { Given, When, Then, setDefaultTimeout } from "@cucumber/cucumber";
+import { chromium,Page,Browser,expect } from "@playwright/test";
 
-Given('User navigates to the application', function () {
-           // Write code here that turns the phrase above into concrete actions
-          console.log("Hi Iam implemented")
-         });
+let browser : Browser;
+let page : Page;
+Given('User navigates to the application', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    // Launch the browser
+    browser = await chromium.launch({ headless: false });
+
+    // Create a new page and await its resolution
+    page = await browser.newPage();
+
+    // Navigate to the application (if needed)
+    await page.goto('https://storytale.io/');
+  });
+
+// ?And User click on the login link
+
+  Given('User click on the login link', async function () {
+    // Write code here that turns the phrase above into concrete actions
+   page.locator('normalize-space()').click
+  });
+
+// ? And User enter the username as "contactkkiran@gmail.com"
+
+  Given('User enter the username as {string}', async function (username) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+// ? And User enter the password as "Varma@143"
+
+  Given('User enter the password as {string}', async function (password) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+// ?When User click on the login button
+
+  When('User click on the login button', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+//  Then Login should be success
+
+  Then('Login should be success', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+// Scenario: Login should not be success # src\test\features\login.feature:13
+// Given User navigates to the application
+
+  Given('User navigates to the application', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+// ? And User click on the login link
+
+  Given('User click on the login link', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+//  Given User enter the username as "Dummy@gmail.com"
+
+  Given('User enter the username as {string}', async function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+//  Given User enter the password as "Dummy"
+
+  Given('User enter the password as {string}', async function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+//  When User click on the login button
+
+  When('User click on the login button', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+// But Login should fail and should show error message
+
+  When('Login should fail and should show error message', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
